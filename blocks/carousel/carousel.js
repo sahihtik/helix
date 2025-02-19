028e301c16a109b3ff8fa8d590d28af7c54e07a2
sahihtikgspann <sahithi.korrakuti@gspann.com>
2025-02-20 00:14:18 +0530

    sdsd
    sd

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,31 +1,21 @@
 export default function decorate(block) {
-  const buttons = document.createElement('div');
-  buttons.className = 'carousel-buttons';
-  block.parentElement.append(buttons);
-  
-  [...block.children].forEach((row, i) => {
-    // Add classes to contents
-    row.children[0].classList.add('carousel-image');
-    row.children[1].classList.add('carousel-text');
-    
-    // Create navigation button
-    const button = document.createElement('button');
-    if (i === 0) button.classList.add('selected');
-    
-    // Add click handler
-    button.addEventListener('click', () => {
-      // Scroll to this slide
-      block.scrollTo({
-        left: row.offsetLeft - block.offsetLeft,
-        behavior: 'smooth'
+    const buttons = document.createElement('div');
+    buttons.className = 'carousel-buttons';
+    [...block.children].forEach((row, i) => {
+      const classes = ['image', 'text'];
+      classes.forEach((e, j) => {
+        row.children[j].classList.add(`carousel-${e}`);
       });
-      
-      // Update button states
-      buttons.querySelectorAll('button').forEach(b => 
-        b.classList.remove('selected'));
-      button.classList.add('selected');
+      /* buttons */
+      const button = document.createElement('button');
+      button.title = 'Carousel Nav';
+      if (!i) button.classList.add('selected');
+      button.addEventListener('click', () => {
+        block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
+        [...buttons.children].forEach((r) => r.classList.remove('selected'));
+        button.classList.add('selected');
+      });
+      buttons.append(button);
     });
-    
-    buttons.append(button);
-  });
-}
+    block.parentElement.append(buttons);
+  }
