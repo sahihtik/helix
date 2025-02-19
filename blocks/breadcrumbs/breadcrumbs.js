e8a5f79d9e381ae8a4d5f519468f310964b28a38
sahihtikgspann <sahithi.korrakuti@gspann.com>
2025-02-19 15:53:46 +0530

    Update breadcrumbs.js

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,33 +1,37 @@
 
-import { getMetadata } from '../../scripts/lib-franklin.js';
+// import { getMetadata } from '../../scripts/lib-franklin.js';
 
-/**
- *
- * @param {HTMLElement} $block The main element
- */
-export default function decorate($block) {
-  console.log("Breadcrumb is calling");
-  const title = getMetadata('og:title');
-  const $ul = document.createElement('ul');
-  $block.append($ul);
-  const trail = [{
-    text: 'Adventures',
-    link: '/adventures',
-  }, {
-    text: title,
-  }];
-  while (trail.length) {
-    const step = trail.shift();
-    const $li = document.createElement('li');
-    $ul.append($li);
-    let $wrap = $li;
-    if (step.link) {
-      $wrap = document.createElement('a');
-      $wrap.href = step.link;
-      $li.append($wrap);
-    }
-    const $span = document.createElement('span');
-    $wrap.append($span);
-    $span.textContent = step.text;
-  }
+// /**
+//  *
+//  * @param {HTMLElement} $block The main element
+//  */
+// export default function decorate($block) {
+//   console.log("Breadcrumb is calling");
+//   const title = getMetadata('og:title');
+//   const $ul = document.createElement('ul');
+//   $block.append($ul);
+//   const trail = [{
+//     text: 'Adventures',
+//     link: '/adventures',
+//   }, {
+//     text: title,
+//   }];
+//   while (trail.length) {
+//     const step = trail.shift();
+//     const $li = document.createElement('li');
+//     $ul.append($li);
+//     let $wrap = $li;
+//     if (step.link) {
+//       $wrap = document.createElement('a');
+//       $wrap.href = step.link;
+//       $li.append($wrap);
+//     }
+//     const $span = document.createElement('span');
+//     $wrap.append($span);
+//     $span.textContent = step.text;
+// 
+
+
+export default function decorate(block) {
+ console.log("breadcrumb is calling")
 }
