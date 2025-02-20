bf4a181fc9ebd580ee9fa23316ea6c84acccb63f
sahihtikgspann <sahithi.korrakuti@gspann.com>
2025-02-20 15:05:14 +0530

    a
    a

--- blocks/new block/new block.js
+++ blocks/new block/new block.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
     [...block.children].forEach((row)=>{
-        row.classList.add("list");
+        row.classList.add("assf");
+        console.log("new-block");
 
     })
     
