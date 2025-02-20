c993a7cc71d365163dceb9ce3828b5044dffe5c4
sahihtikgspann <sahithi.korrakuti@gspann.com>
2025-02-20 17:28:33 +0530

    a
    a

--- blocks/timeline/timeline.js
+++ blocks/timeline/timeline.js
@@ -1,6 +1,8 @@
 export default function decorate(block) {
     [...block.children].forEach((row)=>{
+        [...row.children].forEach((col) => {
         row.classList.add("timeline");
         
     })
+})
 }
